703dc4ea22d661c794ce9310fd11d40358cee95a
Adekabang <iban.ninja@gmail.com>
2024-10-18 03:52:43 -0400

    remove unused faceregister

--- app/components/FaceRegister.tsx
+++ app/components/FaceRegister.tsx
@@ -1,15 +1,8 @@
 
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import React, { useCallback, useRef } from 'react'
 import Webcam from 'react-webcam'
 import Image from 'next/image';
-import { Form, FormDescription, FormLabel, FormMessage } from '@/components/ui/form';
-import { FormControl } from '@/components/ui/form';
-import { FormField, FormItem } from '@/components/ui/form';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
 
 export default function FaceRegister({
     setUserImageB64,
@@ -31,42 +24,6 @@ export default function FaceRegister({
         [webcamRef]
     );
 
-    const formSchema = z.object({
-        username: z.string().min(2).max(50),
-    })
-
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            username: "",
-        },
-    })
-
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values)
-        fetch('/api/users/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                userImageB64: userImageB64
-            })
-        }).then(async (res) => {
-            if (res.status === 201) {
-                const result = await res.json()
-                if (!result.error) {
-                    alert('Successfully registered.')
-                } else {
-                    alert(result.error)
-                }
-                setUserImageB64('')
-            }
-        }).finally(() => {
-            // setLoading(false)
-        })
-    }
-
     return (
         <div className='flex flex-col gap-4'>
             {!userImageB64 &&
@@ -86,36 +43,10 @@ export default function FaceRegister({
                     <Button type='button'
                         onClick={takePhoto}>Take photo</Button>
                 </>}
-
             {userImageB64 && <>
                 <Image src={userImageB64} width={webcamDimensions.width} height={webcamDimensions.height} alt="User photo" />
                 <Button type='button' variant="outline" onClick={() => setUserImageB64('')}>Take another photo</Button>
             </>}
-
-            {/* <Input type="text" placeholder="Enter your name" value={userName} onChange={(e) => setUserName(e.target.value)} /> */}
-
-            {/* <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                    <FormField
-                        control={form.control}
-                        name="username"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Name</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="John Doe" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your public display name.
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <Button type="submit">Submit</Button>
-                </form>
-            </Form> */}
-
         </div>
     )
 }
