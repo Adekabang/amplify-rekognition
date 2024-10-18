65694c993940084eec2039d282f0c53630170ccd
Adekabang <iban.ninja@gmail.com>
2024-10-18 02:00:55 -0400

    change create auth challenge to face_id

--- amplify/auth/create-auth-challenge/handler.ts
+++ amplify/auth/create-auth-challenge/handler.ts
@@ -1,17 +1,17 @@
 import type { CreateAuthChallengeTriggerHandler } from "aws-lambda";
+import { CognitoIdentityProvider } from "@aws-sdk/client-cognito-identity-provider";
+import outputs  from "../../../amplify_outputs.json";
 
 export const handler: CreateAuthChallengeTriggerHandler = async (event) => {
     if (event.request.challengeName === "CUSTOM_CHALLENGE") {
-    // Generate a random code for the custom challenge
-    const challengeCode = "123456";
+        const challengeCode = event.request.userAttributes["custom:face_id"];
 
-    event.response.challengeMetadata = "TOKEN_CHECK";
+        event.response.challengeMetadata = "TOKEN_CHECK";
 
-    event.response.publicChallengeParameters = {
-        trigger: "true",
-        code: challengeCode,
-    };
-
+        event.response.publicChallengeParameters = {
+            trigger: "true",
+            code: challengeCode,
+        };
         event.response.privateChallengeParameters = { trigger: "true" };
         event.response.privateChallengeParameters.answer = challengeCode;
     }
